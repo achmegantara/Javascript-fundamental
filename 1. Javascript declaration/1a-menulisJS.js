04b9b53850a52ab0a4fe16a25b070510d78b6a34
Achmad Akbar Megantara <48613329+achmegantara@users.noreply.github.com>
2021-02-14 20:59:28 +0700

    Javascript declaration

---
+++ 1. Javascript declaration/1a-menulisJS.js
@@ -0,0 +1,3 @@
+var x = 10;
+console.log("Hello world");
+console.log("isi variable dari x = " + x);
---
+++ 1. Javascript declaration/2-popup.js
@@ -0,0 +1,9 @@
+//alert("hello world");
+
+// var nama;
+// nama = prompt("masukkan nama : ");
+// console.log(nama);
+
+var konfirmasi;
+konfirmasi = confirm("ya?");
+console.log(konfirmasi);
